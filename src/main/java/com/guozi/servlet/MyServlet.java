0c2ae4588d905ae026cca8c36e264cf3f9912c7a
guozihao <1025569703@qq.com>
2019-12-20 23:56:32 +0800

    Initial commit

---
+++ src/main/java/com/guozi/servlet/MyServlet.java
@@ -0,0 +1,14 @@
+package com.guozi.servlet;
+
+import java.io.IOException;
+
+public class MyServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        System.out.println("post");
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        System.out.println("get");
+        request.getRequestDispatcher("/hello.jsp").forward(request,response);
+    }
+}
---
+++ src/main/webapp/hello.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: GuoZi
+  Date: 2019/12/19
+  Time: 16:38
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+    <body>
+<a>hello maven hello idea</a>
+</body>
+</head>
+<body>
+
+</body>
+</html>
